e6730fcb47060847254f6b862c0babb169b425d5
Wataru-Nakata <wataru9871@gmail.com>
2015-12-30 21:14:34 +0900

    Winte vacation custom.Bugs are already exist.
    Signed-off-by: Wataru-Nakata <wataru9871@gmail.com>

--- build/source/ProcessingProgram.java
+++ build/source/ProcessingProgram.java
@@ -17,31 +17,40 @@ import java.io.IOException;
 
 public class ProcessingProgram extends PApplet {
 
+//Imports
 
 
+//Defines
+public static final int NumOfTarget= 4;
 Client myClient;
 Serial serial,robot;
-int cam = 123;
+int cam = 123,targetcount = 0;
 PImage img;
 float Fx = 0,Fy = 0,x = 0,y=0,z=0;
 char put = 0;
-target target1 = new target();
+//target initialization
+target[] target = new target[NumOfTarget];
+//Program init
 public void setup()
 {
+  for(int i = 0; i < NumOfTarget; i++)
+  {
+    target[i] = new target();
+  }
   size(1280,720);
   frameRate(30);
   colorMode(RGB);
-  myClient = new Client(this,"192.168.0.4",55555);
-  serial = new Serial(this, "COM3", 9600);
-  robot = new Serial(this,"COM8", 115200);
+  myClient = new Client(this,"192.168.13.4",55555);
+  //serial = new Serial(this, "COM3", 9600);
+  //robot = new Serial(this,"COM8", 115200);
   img = loadImage("img_0.jpg");
 }
-
+//Main loop
 public void draw()
 {
-  if(serial.available()>0){
+  /*jf(serial.available()>0){
     cam =serial.read()-122;
-  }
+  }*/
   if (myClient.available() > 0) {
     String dataIn = myClient.readStringUntil('\n');
 
@@ -61,33 +70,36 @@ public void draw()
               z = PApplet.parseFloat(data[5]);
               float camtheta = (cam/123.0f)*PI/4.0f;
               RelativeToAbs(camtheta);
-              Fx = x;
-              Fy = z;
+              /*Fx = x;
+              Fy = z;*/
+              Fx = 100;
+              Fy = 1000;
               image(img,Fx+width/2-50,Fy-500-50,100,100);
               fill(0, 102, 153);
               textSize(26);
               text(Fx,100,50);
               text(Fy,100,100);
               text("Tracking Object",Fx+width/2+100, Fy-500);
-              serial.write((int)(px/640.0f*255));
+              //serial.write((int)(px/640.0*255));
             }
         }
       }
     }
-    target1.x = 0;
-    target1.y = 1000;
-    ellipse(target1.x+width/2,target1.y-500,100,100);
-    target1.GetRelativePolarCordinates(Fx,Fy);
+    target[targetcount].x = 0;
+    target[targetcount].y = 1000;
+    ellipse(target[targetcount].x+width/2,target[targetcount].y-500,100,100);
+    target[targetcount].GetRelativePolarCordinates(Fx,Fy);
     strokeWeight(10);
     //text(degrees(-target1.theta),100,100);
-    line(Fx+width/2,Fy-500,(100*cos(-target1.theta+PI/2)+Fx+width/2),100*sin(-target1.theta+PI/2)+Fy-500);
+    line(Fx+width/2,Fy-500,(100*cos(-target[targetcount].theta+PI/2)+Fx+width/2),100*sin(-target[targetcount].theta+PI/2)+Fy-500);
     /*robot.write(255);*/
-    robot.write(PApplet.parseInt((target1.theta+PI)/(2*PI)*254));
-    text(PApplet.parseInt((target1.theta+PI)/(2*PI)*254),100,150);
+    //robot.write(int((target1.theta+PI)/(2*PI)*254));
+    text(PApplet.parseInt((target[targetcount].theta+PI)/(2*PI)*254),100,150);
+    text(targetcount,100,200);
     /*if (target1.r> 200){
       put = 200;
-    }
-    robot.write(put);*/
+    }*/
+    //robot.write(put);
     myClient.clear();
   }
 }
@@ -113,6 +125,9 @@ public void RelativeToAbs(float theta2)
   tempy = z;
   x = tempx*cos(theta2)-tempy*sin(theta2);
   z = tempx*sin(theta2)+tempy*cos(theta2);
+}
+public void mouseClicked(){
+  targetcount+= 1;
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "ProcessingProgram" };
